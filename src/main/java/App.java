40fa8ba8018ff02b347e415f78329e681e38e908
Frédéric Delorme <frederic.delorme@versusmind.eu>
2021-12-09 15:43:51 +0100

    feat(props): Add properties data

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,18 +1,34 @@
+import java.util.ResourceBundle;
+
 /**
  * This the MyPorject main class
  *
  * @author Frédéric Delorme
  * @since 0.0.1
  */
-public class App{
+public class App {
+
+  ResourceBundle config;
+  String title;
+
+  public App() {
+    config = ResourceBundle.getBundle("app");
+    title = config.getString("title");
+  }
+
+  public void displayMsg(String msg) {
+    System.out.println(String.format("%s: %s", title, msg));
+  }
 
   /**
    * The main entry point fo this program.
    *
    * @param argc the list of command line arguments send to java program
    */
-  public static void main(String[] argc){
-    System.out.println("Hello World, I am my-project main App class");
+  public static void main(String[] argc) {
+    App app = new App();
+    app.displayMsg("Hello World, I am my-project main App class");
+
   }
 
 }
